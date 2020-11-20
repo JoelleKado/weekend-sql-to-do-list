6b233c36ab3acac814094e7142fc638a683594cf
Joelle Kado <jkado@my.hpu.edu>
2020-11-20 16:05:56 -0600

    POST works

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -18,21 +18,23 @@ router.get('/', (req, res) => {
 
 // Adds a new book to the list of awesome reads
 // Request body must be a book object with a title and author.
-router.post('/',  (req, res) => {
-  let newBook = req.body;
-  console.log(`Adding book`, newBook);
 
-  let queryText = `INSERT INTO "books" ("author", "title")
-                   VALUES ($1, $2);`;
-  pool.query(queryText, [newBook.author, newBook.title])
-    .then(result => {
-      res.sendStatus(201);
-    })
-    .catch(error => {
-      console.log(`Error adding new book`, error);
-      res.sendStatus(500);
-    });
+router.post('/',  (req, res) => {
+  let newTask = req.body;
+  console.log('adding task', newTask)
+
+  //let queryText = `INSERT INTO "books" ("author", "title")
+              //     VALUES ($1, $2);`;
+  //pool.query(queryText, [newBook.author, newBook.title])
+    // .then(result => {
+    res.sendStatus(201);
+    //  })
+    // .catch(error => {
+    //   console.log(`Error adding new book`, error);
+    //   res.sendStatus(500);
+    // });
 });
+
 
 // TODO - PUT
 // Updates a book to show that it has been read
