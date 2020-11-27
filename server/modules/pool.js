8361789260ff8269f789b08e0099a45af83149fe
Joelle Kado <jkado@my.hpu.edu>
2020-11-27 09:25:17 -0600

    upated pool config for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,21 +1,46 @@
 const pg = require('pg');
 
-const config = {
+
+const url = require('url');
+let config = {};
+
+
+
+//get the Pool object from pg
+const Pool = pg.Pool
+
+//make our own instance of a Pool from that template Pool object
+  //this says how to connect to the Db 
+
+if (process.env.DATABASE_URL) {
+  //running remote (heroku)
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthenticated: false}
+  }
+
+} else {
+  //running locally
+  
+
+config = {
   database: 'weekend-to-do-app', 
   host: 'localhost', 
   port: 5432, 
   max: 10, 
   idleTimeoutMillis: 30000 
 };
+}
 
-const pool = new pg.Pool(config);
+// create the pool with the proper config
+const pool = new Pool(config);
 
 pool.on("connect", () => {
   console.log("connected to postgres");
 });
 
 pool.on("error", (err) => {
-  console.log("error connecting to postgres", err);
+    console.log("error connecting to postgres", err);
 });
 
 module.exports = pool;
